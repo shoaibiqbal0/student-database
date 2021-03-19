5cac47cb92efa46f1ffbbcd4456150f68e3983a4
Shoaib Iqbal <shoaibiqbal@gmail.com>
2021-03-19 21:01:56 +0000

    Update

--- static/index.js
+++ static/index.js
@@ -1,6 +1,20 @@
 "use strict";
 
-function printStudents(){
-    var main = document.getElementById("main");
-    main.innerHTML = "Hello World!";
+class Student{
+    id;
+    name;
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+    }
 }
+
+var mainApp = angular.module("mainApp", []);
+
+mainApp.controller("studentController", function($scope){
+    $scope.student = new Student("001", "Shoaib Iqbal");
+    $scope.update = function(){
+        $scope.student.id = "002";
+        $scope.student.name = "Iqbal Ahmad";
+    }
+});
